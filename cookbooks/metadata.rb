1e4d74a63ac8866f345ade1d6691f8e1e593106b
mlennon3@gmail.com <mlennon3@gmail.com>
2015-06-16 09:37:26 -0700

    Michael - add grunt-cli and git to box

--- cookbooks/metadata.rb
+++ cookbooks/metadata.rb
@@ -3,3 +3,5 @@ version "0.1.0"
 
 depends 'nodejs', '~> 2.4.0'
 depends 'couchbase', '~> 1.3.1'
+depends 'git', '~> 4.2.2'
+
--- cookbooks/recipes/default.rb
+++ cookbooks/recipes/default.rb
@@ -3,9 +3,12 @@ include_recipe "nodejs"
 
 include_recipe "couchbase::server"
 include_recipe "couchbase::buckets"
-
+include_recipe "git"
 
 nodejs_npm "forever"
 
+nodejs_npm "grunt-cli" do
+  options ['--save-dev']
+end
 
 
